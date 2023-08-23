07b0685f3822aec4b69da2845728270bf3511480
Sudin Shakya <sudshakya@gmail.com>
2023-08-24 09:59:19 +1000

    first Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React 
+          Hello React Sudin
         </p>
         <a
           className="App-link"
