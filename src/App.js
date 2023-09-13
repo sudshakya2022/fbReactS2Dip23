4d439e8259568dfd69e9bca67172fd3b5bd87b0a
Sudin Shakya <sudshakya@gmail.com>
2023-09-13 12:17:00 +1000

    userAdded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import { FirebaseConfig } from "./config/Config"
-import {initializeApp} from "firebase/app"
-import { Routes, Route} from "react-router-dom"
+import { FirebaseConfig } from "./config/Config";
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
-import { Header } from "./components/Header"
-import './App.css';
-import { About } from "./pages/About"
-import { Home } from "./pages/Home"
-import { Contact } from "./pages/Contact"
-
+import { Header } from "./components/Header";
+import "./App.css";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Signup } from "./pages/Signup";
 
 function App() {
-  const FBapp = initializeApp(FirebaseConfig)
+  const FBapp = initializeApp(FirebaseConfig);
+  const FBauth = getAuth();
+  // navigation array
+  const NavItems = [
+    { label: "Home", link: "/" },
+    { label: "About", link: "/about" },
+    { label: "Contact", link: "/contact" },
+    { label: "Create Account", link: "/signup" },
+    { label: "Log in", link: "/signin" },
+  ];
+  // navigation for authenticated user
+
+  const AuthNavItems = [
+    { label: "Home", link: "/" },
+    { label: "About", link: "/about" },
+    { label: "Contact", link: "/contact" },
+    { label: "Log out", link: "/signout" },
+  ];
+  // application states
+  const [nav, setNav] = useState(NavItems);
+  //signing up a user
+  const signUp = (email, password) => {
+    createUserWithEmailAndPassword(FBauth, email, password)
+      .then((userCredential) => {})
+      .catch((error) => console.log(error.message));
+  };
   return (
     <div className="App">
-    <Header/>
+      <Header items={nav} />
       <Routes>
-      <Route path="/" element={ <Home greeting="You are in Home Page!" /> } />
-        <Route path="/about" element={ <About greeting="Hey you, this is about page!"/>} />
-        <Route path="/contact" element={ <Contact greeting="You are in Contact Page!" /> } />
+        <Route path="/" element={<Home greeting="You are in Home Page!" />} />
+        <Route
+          path="/about"
+          element={<About greeting="Hey you, this is about page!" />}
+        />
+        <Route
+          path="/contact"
+          element={<Contact greeting="You are in Contact Page!" />}
+        />
+        <Route path="/signup" element={<Signup handler={signUp} />} />
       </Routes>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,19 @@ import  Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 
 export function  Header ( props ){
+  // props.item is the value of nav state in App.js
+  // create a collection of navigation items
+  const Links = props.items.map( (item) => {
+    return (
+      <Nav.Link href={item.link}> {item.label} </Nav.Link>
+    )
+  } )
     return(
         <Navbar>
       <Container>
       <Navbar.Brand>App</Navbar.Brand>
       <Nav>
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/about">About</Nav.Link>
-        <Nav.Link href="/contact">Contact</Nav.Link>
+        {Links}
       </Nav>
 
       </Container>
